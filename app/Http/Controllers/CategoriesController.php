a51d1046e33cabd5dfa43c7da6962065d2caef8b
lrn80 <1349539656@qq.com>
2020-06-08 01:49:16 +0000

    feat: 侧边栏链接

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -10,7 +11,7 @@ use Illuminate\Http\Request;
 class CategoriesController extends Controller
 {
     //
-    public function show(Category $category, Request $request,Topic $topic, User $user)
+    public function show(Category $category, Request $request,Topic $topic, User $user, Link $link)
     {
         // 读取分类 ID 关联的话题，并按照每20条进行分类
         $topics = $topic->withOrder($request->order)
@@ -19,7 +20,8 @@ class CategoriesController extends Controller
         // 活跃用户列表
         $active_users = $user->getActiveUsers();
 
+        $links = $link->getAllCached();
         // 传参变量话题和分类到模版中
-        return view('topics.index', compact('topics', 'category', 'active_users'));
+        return view('topics.index', compact('topics', 'category', 'active_users', 'links'));
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -41,7 +41,7 @@ class UsersTableSeeder extends Seeder
         // 单独处理第一个用户的数据
         $user = User::find(1);
         $user->name = 'liruoning';
-        $user->email = 'lrn80@qq.com';
+        $user->email = 'lrn98@qq.com';
         $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/NDnzMutoxX.png';
         $user->save();
 
